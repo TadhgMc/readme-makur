7971d20a9c290e293e76995831cbfe6b61a56abd
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-03-26 11:41:15 -0400

    cleaning js and using app to make readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,55 +1,41 @@
-// TODO: Include packages needed for this application
-// 'npm init -y'
-// 'npm i inquirer (--save ?)'
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-
-//START
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer
     .prompt([
-        {type: 'input', name: 'title', message: 'What is the title of your app?',},
-        {type: 'input', name: 'description', message: 'What would you like for your description to say?',},
-        {type: 'input', name: 'tableOfContents', message: 'What elements would you like to be in the table of contents?',}, //need to specify somewhere how to enter each item, ie.. each section with a space betweeen ?
-        {type: 'input', name: 'installation', message: 'What instructions do you have for installation information?',},
-        {type: 'input', name: 'usage', message: 'Please describe how to use your app.',},
-        {type: 'list', name: 'license', message: 'Please choose any licenses you used.', choices: ['None', 'MIT', 'something else'] },
-        {type: 'input', name: 'contributions', message: 'Who made the app the readme will be for?',},
+        {type: 'input', name: 'title', message: 'What is the title of your app?'},
+        {type: 'input', name: 'description', message: 'What would you like for your description to say?'},
+        {type: 'input', name: 'tableOfContents', message: 'What elements would you like to be in the table of contents?'}, //need to specify somewhere how to enter each item, ie.. each section with a space betweeen ?
+        {type: 'input', name: 'installation', message: 'What instructions do you have for installation information?'},
+        {type: 'input', name: 'usage', message: 'Please describe how to use your app.'},
+        {type: 'list', name: 'license', message: 'Please choose any licenses you used.', choices: ['None', 'MIT', 'GNU'] },
+        {type: 'input', name: 'contributions', message: 'Who made the app the readme will be for?'},
     ])
     .then((data) => {
-        const readme = `# ${data.title}
-        ## Description
-        ${data.description}
+        const readme =
+`
+# ${data.title}
 
-        ## Table of Contents
-        ${data.tableOfContents}
+## Description
+${data.description}
 
-        ## Installation
-        ${data.installation}
+## Table of Contents
+${data.tableOfContents}
 
-        ## Usage
-        ${data.usage}
+## Installation
+${data.installation}
 
-        ## Licenses
-        ${data.license}
+## Usage
+${data.usage}
 
-        ## Contributers
-        ${data.contributions}
-        `
-        console.log(readme)
-        console.log(data.tableOfContents[0]);
-        console.log(data.tableOfContents[1]);
-        //fs.writeFile('nameOfFile', readme, (err) ? console.log(err) : console.log("All Good!"));
+## Licenses
+${data.license}
+
+## Contributers
+${data.contributions}
+`;
+
+        fs.writeFile('ReadmeTest.md', readme, (err) =>
+           err ? console.error(err) : console.log('Success!'));
+        console.log(readme);
     });
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# readme-makur
-Command-line app that makes customizable readmes!
+# ReadMe Maker
+
+## Description
+This app has been made to quickly help create professional readmes!
+
+## Installation
+Please run the commands 
+
+``` bash
+npm init
+```
+ and then 
+
+ ``` bash
+ npm i inquirer
+ ```
+
+## Usage
+After the installations, you begin the code by running the command 'node index.js' and then answer the questions with the content you would like to fill each section of a professional readme. Afterwards, the app will create a readme with all the information you provided!
+
+## Licenses
+None
+
+## Contributers
+Just me
